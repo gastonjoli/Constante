55574d245281219807618d43873ececf8330cd45
Gaston Jolinont <falsee63@gmail.com>
2021-10-01 09:14:40 -0300

    Aprendiendo Constantes

---
+++ src/Practica/Practica.java
@@ -0,0 +1,16 @@
+
+// Constantes
+//Espacio de memosria donde puedo almacenar un valor.
+//Las constantes NO PUEDEN cambiar su valor a lo largo del programa.
+package Practica;
+
+public class Practica {
+    public static void main(String[] args) {
+
+        final int numero = 10; // constante (Final)
+
+        System.out.println( numero);
+
+
+    }
+}
